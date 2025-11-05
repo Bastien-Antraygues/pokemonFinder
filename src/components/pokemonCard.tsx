c1675dac81e7bdba1142df5f6d77ee8085116138
Bastien ANTRAYGUES <bastien-toulouse@outlook.fr>
2025-11-05 16:30:06 +0100

    Add favorites heart need to implement provider

--- src/components/pokemonCard.tsx
+++ src/components/pokemonCard.tsx
@@ -1,14 +1,25 @@
+import { useState } from "react"
 import { useFetch } from "../customHooks/customHooks"
 import type { Pokemon } from "../interfaces/Pokemon"
 import { Type } from "./Type"
 
 export function PokemonCard(props:{url:string}){
     const data : Pokemon | null = useFetch<Pokemon>(props.url)
+    const [isFavorite, setIsFavorite] = useState(false)
     if(data){
         return(
             <>
         
             <div className="m-4 p-4 border-1 text-center w-60 mx-auto bg-red-50">
+                <div className="text-right">
+                    <button
+                        className=""
+                        onClick={() => setIsFavorite(!isFavorite)}
+                        aria-label="Ajouter ou retirer des favoris"
+                    >
+                    {isFavorite ? "❤️" : "🤍"}
+                    </button>
+                </div>
                 <h1 className="">{data.name}</h1>
             
                 <img className="mx-auto" src={data.sprites.front_default} alt="" />
