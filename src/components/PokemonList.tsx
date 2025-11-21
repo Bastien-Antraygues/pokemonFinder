68037efa437944122b7c5cc991373b2a02b297fa
Bastien ANTRAYGUES <bastien-toulouse@outlook.fr>
2025-11-22 00:23:02 +0100

    PokeList add pageableComponent use

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,14 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { PokeSize } from "../interfaces/PokeSize";
 import { PokemonCard } from "./PokemonCard";
+import { PageableComponent } from "./PageableComponent";
 // max page 66
-export function PokemonList(props: { pokeList: { name: string, url: string }[],pokeSize:PokeSize }) {
-    
+export function PokemonList(props: { pokeList: { name: string, url: string }[], isPageable:boolean }) {
+
+    const [page, setPage] = useState<number>(1)
+    const [pageable, setPageable] = useState<{ pStart: number, pEnd: number }>(getPokeSize(page))
+
+    useEffect(()=>{
+        setPage(1)
+        setPageable(getPokeSize(1))
+    },[props.pokeList])
+
     return (
         <>
-            <div className="grid lg:grid-cols-2 xl:grid-cols-4">
+        {
+            props.isPageable ?
+            <ul className="flex justify-center">
+                <li>
+                    {
+                        getPage(page).map((pageNumber) => {
+                            return <PageableComponent key={pageNumber} pageNumber={pageNumber} isPage={page == pageNumber}
+                                onSelectPage={onSelectPage}
+                            />
+                        })
+                    }
+                </li>
+
+            </ul> : <></>
+        }
+            
+            <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
                 {
-                    props.pokeList.slice(props.pokeSize.pStart, props.pokeSize.pEnd).map((result) => {
+                    props.pokeList.slice(pageable.pStart, pageable.pEnd).map((result) => {
                         const id = result.url.split("/").filter(Boolean).pop();
                         return <PokemonCard key={id} url={result.url} />
                     })
@@ -18,5 +43,46 @@ export function PokemonList(props: { pokeList: { name: string, url: string }[],p
 
     )
 
-    
+    function onSelectPage(sPage: number) {
+        setPage(sPage)
+        setPageable(getPokeSize(sPage))
+    }
+}
+
+function getPage(pageNumber: number) {
+    let pageStart: number = pageNumber
+    let pageEnd: number = pageNumber
+    if (pageNumber < 66) {
+        for (let i = pageNumber; i < pageNumber + 3; i++) {
+            pageEnd = i
+            if (i == 66) {
+                break
+            }
+        }
+    }
+    if (pageNumber > 1) {
+        for (let i = pageNumber; i > pageNumber - 3; i--) {
+            pageStart = i
+            if (i == 1) {
+                break
+            }
+        }
+    }
+    let tab: number[] = []
+    for (let i = pageStart; i <= pageEnd; i++) {
+        tab.push(i)
+    }
+    if (pageStart > 1) {
+        tab = [1].concat(tab)
+    }
+    if (pageEnd < 66) {
+        tab.push(66)
+    }
+    return tab
+}
+
+export function getPokeSize(pageNumber: number): PokeSize {
+    let pEnd = pageNumber * 20
+    let pStart = pEnd - 20
+    return { pStart: pStart, pEnd: pEnd }
 }
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -2,12 +2,8 @@ import { useEffect, useState } from "react";
 import { useFetch } from "../customHooks/customHooks";
 import type { Page } from "../interfaces/Page";
 import { PokemonList } from "../components/PokemonList";
-import type { PokeSize } from "../interfaces/PokeSize";
-import { PageableComponent } from "../components/PageableComponent";
 // 1328
 export function Search() {
-    const [page,setPage] = useState<number>(1)
-    const [pageable,setPageable] = useState<{pStart:number,pEnd:number}>(getPokeSize(page))
     const [search, setSearch] = useState("")
     const [filterResults, setFilterResults] = useState<{ name: string; url: string }[]>([])
     let data: Page | null = useFetch<Page>("https://pokeapi.co/api/v2/pokemon/?limit=1302")
@@ -15,10 +11,7 @@ export function Search() {
         if (data) {
             console.log(search)
             if (search && search.length > 2) {
-                if(page!=1){
-                    setPage(1)
-                    setPageable(getPokeSize(1))
-                }
+                
                 setFilterResults(data.results.filter(result => result.name.toLowerCase().includes(search.toLowerCase())))
             } else {
                 setFilterResults(data.results)
@@ -29,34 +22,18 @@ export function Search() {
 
     return (
         <>
-            
-            <input type="text" value={search} onInput={(e) => setSearch(e.target.value)} 
-            className="ml-18 p-2 bg-pink-3 border-2 border-pink-6 w-80" placeholder="Recherche par nom de pokemon" />
+
+            <input type="text" value={search} onInput={onImput}
+                className="ml-18 p-2 bg-pink-3 border-2 border-pink-6 w-80" placeholder="Recherche par nom de pokemon" />
 
             {
                 data ? (
                     <>
-                        <PokemonList pokeList={filterResults} pokeSize={pageable} />
-                        {
-                            search.length < 3 ? <ul className="flex justify-center">
-                            <li>
-                               {
-                            getPage(page).map((pageNumber)=>{
-                                return <PageableComponent key={pageNumber} pageNumber={pageNumber} isPage={page==pageNumber}
-                                onSelectPage={onSelectPage}
-                                />
-                            })
-                        }    
-                            </li>
-                         
-                        </ul> : <></>
-                        }
-                        
-                        
+                        <PokemonList pokeList={filterResults} isPageable={search.length<3}/>
                     </>
-                    
-                    
-                    
+
+
+
                 ) : (
                     <p>Chargement des Pokémon...</p>
                 )
@@ -65,48 +42,9 @@ export function Search() {
         </>
     )
 
-    function onSelectPage(sPage:number){
-        setPage(sPage)
-        setPageable(getPokeSize(sPage))
-    }
-
-    function getPokeSize(pageNumber:number): PokeSize {
-        let pEnd = pageNumber*20
-        let pStart = pEnd-20
-        return {pStart:pStart,pEnd:pEnd}
-    }
-
-    function getPage(pageNumber:number){
-        let pageStart:number = pageNumber
-        let pageEnd:number = pageNumber
-        if(pageNumber<66){
-            for (let i = pageNumber; i < pageNumber + 3; i++) {
-                pageEnd = i
-                if (i == 66) {
-                    break
-                }
-            }
-        }
-        if(pageNumber>1){
-            for(let i=pageNumber;i>pageNumber-3;i--){
-                pageStart = i
-                if (i == 1) {
-                    break
-                }
-            }
-        }
-        let tab : number[] = []
-        for(let i = pageStart;i<=pageEnd;i++){
-            tab.push(i)
-        }
-        if(pageStart>1){
-            tab = [1].concat(tab)
-        }
-        if(pageEnd<66){
-            tab.push(66)
-        }
-        console.log(pageEnd)
-        return tab
+    function onImput(e: React.FormEvent<HTMLInputElement>) {
+        setSearch(e.target.value)
     }
 
 }
+
