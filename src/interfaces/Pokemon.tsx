67e20fb70edbfafcea52304d3b4eb6d0fd0691bc
Bastien ANTRAYGUES <bastien-toulouse@outlook.fr>
2025-11-22 00:21:17 +0100

    Add abilities and stats on Pokemon interface

--- src/interfaces/Pokemon.tsx
+++ src/interfaces/Pokemon.tsx
@@ -3,10 +3,24 @@ import type { Types } from "./Types"
 export interface Pokemon{
     id:number,
     name:string,
+    abilities: {
+        ability:{
+            name:string,
+            url:string
+        }
+    }[],
     sprites:{
         front_default:string,
         back_default:string
     },
+    stats:{
+        base_stat:number,
+        effort:number,
+        stat:{
+            name:string,
+            url:string
+        }
+    }[],
     types:[{
         type:{
             name:string,
