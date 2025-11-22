9c5bde5fe7797f0a21a04878289fdf12e255aee0
Bastien ANTRAYGUES <bastien-toulouse@outlook.fr>
2025-11-22 18:42:34 +0100

    PokemonCard refactor code by delete some useState and variable and the useEffect replace by a simple function

--- src/components/pokemonCard.tsx
+++ src/components/pokemonCard.tsx
@@ -10,59 +10,55 @@ export const PokemonCard = React.memo(function (props: { url: string }) {
     const data: Pokemon | null = useFetch<Pokemon>(props.url)
 
     const { favorite, setFavorite } = useFavorites()
-    const initialString: string = favorite.filter(value => value.startsWith(props.url)).toString()
-    const inital: boolean = initialString ? true : false
-    const [isFavorite, setIsFavorite] = useState(inital)
+    const isFavorite = favorite.includes(props.url)
     const navigate = useNavigate()
-    useEffect(() => {
 
-        if (isFavorite && data) {
-            if (!favorite.includes(props.url)) {
-                setFavorite([...favorite, props.url])
-            }
-
-        } else if (!isFavorite && data) {
-            if (favorite.includes(props.url)) {
-
-                setFavorite(favorite.filter((item) => item !== props.url))
-            }
+    function toggleFavorite(){
+        if(isFavorite){
+            setFavorite(favorite.filter(item=>item!==props.url))
+        }else{
+            setFavorite([...favorite,props.url])
         }
-    }, [isFavorite])
+    }
 
-    if (data) {
+    if (!data) {
         return (
             <>
 
-                <div className="m-4 p-4 border-2  text-center w-60 mx-auto border-pink-6 bg-pink-3 rounded-xl">
-                    <div className="text-right">
-                        <button
-                            className=""
-                            onClick={() => setIsFavorite(!isFavorite)}
-                            aria-label="Ajouter ou retirer des favoris"
-                        >
-                            {isFavorite ? "❤️" : "🤍"}
-                        </button>
-                    </div>
-                    <div onClick={()=> navigate("/detail/"+data.id)} className=" p-2 border-pink-8 rounded-lg hover:shadow-2xl">
-                        <h1 className="">{upperName(data.name)}</h1>
+                <h1>Pokemon non trouvé</h1>
 
-                        <img className="mx-auto" src={data.sprites.front_default} alt="" />
-                        <p>N° {data.id}</p>
-                        <div className="flex">{data.types.map((element) => {
-                            return <Type key={element.type.name} name={element.type.name} />
-                        })}</div>
-                    </div>
-
-                </div>
             </>
         )
     }
+
+
     return (
         <>
 
-            <h1>Pokemon non trouvé</h1>
+            <div className="m-4 p-4 border-2  text-center w-60 mx-auto border-pink-6 bg-pink-3 rounded-xl">
+                <div className="text-right">
+                    <button
+                        className=""
+                        onClick={toggleFavorite}
+                        aria-label="Ajouter ou retirer des favoris"
+                    >
+                        {isFavorite ? "❤️" : "🤍"}
+                    </button>
+                </div>
+                <div onClick={() => navigate("/detail/" + data.id)} className=" p-2 border-pink-8 rounded-lg hover:shadow-2xl">
+                    <h1 className="">{upperName(data.name)}</h1>
 
+                    <img className="mx-auto" src={data.sprites.front_default} alt="" />
+                    <p>N° {data.id}</p>
+                    <div className="flex">{data.types.map((element) => {
+                        return <Type key={element.type.name} name={element.type.name} />
+                    })}</div>
+                </div>
+
+            </div>
         </>
     )
+
+
 })
 
