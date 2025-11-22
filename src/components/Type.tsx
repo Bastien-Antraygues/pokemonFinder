05c70dfde06f02afc18b5a3154bdae9bb841bcbf
Bastien ANTRAYGUES <bastien-toulouse@outlook.fr>
2025-11-22 18:39:51 +0100

    remove GetType to change into a Record<string,string> (dictionnaire)

--- src/components/Type.tsx
+++ src/components/Type.tsx
@@ -1,52 +1,30 @@
 export function Type(props:{name:string}){
     
-    function getType(name:string){
-        
-        switch(name){
-            case 'grass':
-                return 'bg-[#9bcc50] text-black'
-            case 'fire':
-                return 'bg-[#fd7d24] text-white'
-            case 'water':
-                return 'bg-[#4592c4] text-white'
-            case 'poison':
-                return 'bg-[#b97fc9] text-white'
-            case 'flying':
-                return 'bg-[#3dc7ef] text-black'
-            case 'bug':
-                return 'bg-[#729f3f] text-white'
-            case 'normal':
-                return 'bg-[#a4acaf] text-black'
-            case 'electric':
-                return 'bg-[#eed535] text-black'
-            case 'sol':
-                return 'bg-[#ab9842] text-black'
-            case 'fairy':
-                return 'bg-[#fdb9e9] text-black'
-            case 'fighting':
-                return 'bg-[#d56723] text-white'
-            case 'psychic':
-                return 'bg-[#f366b9] text-white'
-            case 'rock':
-                return 'bg-[#a38c21] text-white'
-            case 'steel':
-                return 'bg-[#9eb7b8] text-black'
-            case 'ice':
-                return 'bg-[#51c4e7] text-black'
-            case 'ghost':
-                return 'bg-[#7b62a3] text-white'
-            case 'dragon':
-                return 'bg-[#53a4cf] text-white'
-            case 'dark':
-                return 'bg-[#707070] text-white'
-            case 'ground': 
-                return 'bg-[#ab9842] text-black'
-        }
-    }
-    
     return(
         <>
-            <div className={getType(props.name)+" rounded-[3px] w-[38.4375%] leading-8 mx-auto"}>{props.name}</div>
+            <div className={TYPE_STYLES[props.name]+" rounded-[3px] w-[38.4375%] leading-8 mx-auto"}>{props.name}</div>
         </>
     )
 }
+
+const TYPE_STYLES: Record<string, string> = {
+    grass: 'bg-[#9bcc50] text-black',
+    fire: 'bg-[#fd7d24] text-white',
+    water: 'bg-[#4592c4] text-white',
+    poison: 'bg-[#b97fc9] text-white',
+    flying: 'bg-[#3dc7ef] text-black',
+    bug: 'bg-[#729f3f] text-white',
+    normal: 'bg-[#a4acaf] text-black',
+    electric: 'bg-[#eed535] text-black',
+    sol: 'bg-[#ab9842] text-black',
+    fairy: 'bg-[#fdb9e9] text-black',
+    fighting: 'bg-[#d56723] text-white',
+    psychic: 'bg-[#f366b9] text-white',
+    rock: 'bg-[#a38c21] text-white',
+    steel: 'bg-[#9eb7b8] text-black',
+    ice: 'bg-[#51c4e7] text-black',
+    ghost: 'bg-[#7b62a3] text-white',
+    dragon: 'bg-[#53a4cf] text-white',
+    dark: 'bg-[#707070] text-white',
+    ground: 'bg-[#ab9842] text-black',
+};
