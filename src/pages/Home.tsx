ddc76912f123f7996da02e066646fc436bfe1ce7
Bastien ANTRAYGUES <bastien-toulouse@outlook.fr>
2025-12-17 19:30:30 +0100

    Ajout d"un loading et error, utilisations du useeffect pour récupérer les données

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,45 @@
+import { useEffect, useState } from "react"
 import { PokemonCard } from "../components/PokemonCard"
-import { PokemonList } from "../components/PokemonList"
-import { useFetch } from "../customHooks/customHooks"
 import type { Page } from "../interfaces/Page"
-import type { Pokemon } from "../interfaces/Pokemon"
+import api from "../services/api"
 
-export function Home(){
+export function Home() {
     const offset = getRandomInt(1299)
+    const [data, setData] = useState<Page>()
+    const [loading,setLoading] = useState(false)
+    const [error,setError] = useState("")
 
-    const data : Page | null = useFetch<Page>("/api/pokemon/?limit=1302")
-    if(data){
+    useEffect(() => {
+        setLoading(true)
+        api.getPokemons().then(page => {
+            setData(page)
+        }).catch((err)=>setError(err))
+        .finally(()=> setLoading(false))
+    }, [])
+    if(loading){
         return(
-        <>
-            <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {
-                data.results.slice(offset,offset+20).map((page)=>{
-                    return <PokemonCard url={page.url} key={page.name}/>
-                })
-            }
-            </div>
-        </>
-    )
+            <p>Chargement En cours ...</p>
+        )
     }
-    return(
+    if(error){
+        return(
+            <p>Erreur: {error}</p>
+        )
+    }
+    if (data) {
+        return (
+            <>
+                <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                    {
+                        data.results.slice(offset, offset + 20).map((page) => {
+                            return <PokemonCard url={page.url} key={page.name} />
+                        })
+                    }
+                </div>
+            </>
+        )
+    }
+    return (
         <>
             <h1>Page d'Accueil</h1>
             <p>pas de data</p>
@@ -29,6 +47,6 @@ export function Home(){
     )
 }
 
-function getRandomInt(max:number) {
-  return Math.floor(Math.random() * max);
+function getRandomInt(max: number) {
+    return Math.floor(Math.random() * max);
 }
