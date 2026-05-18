c2c03cdcb31b44817045a20b945b6e30b812564e
Bastien Antraygues <bastien-toulouse@outlook.fr>
2026-05-18 16:24:40 +0200

    Ajout de fonction d'appel du back end, login/logout/getMe/refreshToken

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -10,7 +10,18 @@ const api = {
   getPokemonById: async (id:string) => {
     return fetch.get<Pokemon>("/pokemon/"+id).then((res)=>res.data)
   },
-  
+  getNewToken: async () => {
+    return fetch.get("/auth/refreshToken").then((res)=>res.data)
+  },
+  getUserMe: async () => {
+    return fetch.get("/auth/me").then((res)=>res.data)
+  },
+  login: async (email:string, password:string) => {
+    return fetch.post("/auth/login", {email, password}).then((res)=>res.data)
+  },
+  logout: async () => {
+    return fetch.post("/auth/logout").then((res)=>res.data)
+  }
 }
 
 
