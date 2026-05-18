ccf02e6125eee01f40215df125dd209d866af9b7
Bastien Antraygues <bastien-toulouse@outlook.fr>
2026-05-18 16:28:15 +0200

    Login -> ajout de useState email / password et de la fonction handleSubmit qui connect l'utilisateur. Dans le futur, il faut ajouter un message d'erreur lorsque l'authentification fail

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,18 +1,40 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import api from "../services/api";
+import { useState } from "react";
+import { useAuth } from "../providers/AuthProvider";
 
 export function Login(){
+
+    const { login } = useAuth();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+    
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        try {
+            const res = await api.login(email, password);
+            login(res.user, res.token);
+            navigate("/");
+        } catch (error) {
+            console.error("Login failed", error);
+        }
+    }
+
     return(
-        <div className="m-4 p-4 border-2 w-[40%] mx-auto border-pink-6 bg-pink-3 rounded-xl
-        flex flex-col gap-4">
+        <form className="m-4 p-4 border-2 w-[40%] mx-auto border-pink-6 bg-pink-3 rounded-xl
+        flex flex-col gap-4" onSubmit={handleSubmit}>
             <div className="flex gap-1 justify-center">
                 <div className="w-[15%] mt-2"><label htmlFor="email" className="">Email :</label></div>
                 <input type="email" name="email" id="email" placeholder="exemple@gmail.com"
-                className="w-[80%] p-2 border-2 bg-pink-3 border-pink-12 rounded-lg" />
+                className="w-[80%] p-2 border-2 bg-pink-3 border-pink-12 rounded-lg" 
+                value={email} onChange={(e) => setEmail(e.target.value)} />
             </div>
             <div className="flex gap-1 justify-center">
                 <div className="w-[15%] mt-2"><label htmlFor="password" className="">Password :</label></div>
                 <input type="password" name="password" id="password" placeholder="password"
-                className="w-[80%] p-2 border-2 bg-pink-3 border-pink-12 rounded-lg" />
+                className="w-[80%] p-2 border-2 bg-pink-3 border-pink-12 rounded-lg" 
+                value={password} onChange={(e) => setPassword(e.target.value)} />
             </div>
             <div className="flex justify-center">
                 <button className="mr-4 text-pink-12 underline">Forgot Password?</button>
@@ -22,6 +44,6 @@ export function Login(){
                 <button className="bg-pink-8 hover:bg-pink-9 text-white font-bold py-2 px-4 rounded-lg">Login</button>
             </div>
             
-        </div>
+        </form>
     )
 }
