8f37dd99fc8d25011b1e2ac970aefe2b1ec1a74d
Bastien Antraygues <bastien-toulouse@outlook.fr>
2026-05-18 16:26:50 +0200

    AuthProvider ajout d'un useEffect qui update le token si la personne rénitialise la page et était connecter avec un cookie

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,5 +1,7 @@
-import { createContext, use, useState } from "react";
+import { createContext, use, useEffect, useLayoutEffect, useState } from "react";
 import type { User } from "../interfaces/User";
+import api from "../services/api";
+import fetch from "../config/fetch";
 
 type AuthContextType = {
     user: User | null,
@@ -20,6 +22,27 @@ const AuthContext = createContext<AuthContextType>({
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     const [user, setUser] = useState<User | null>(null);
     const [token, setToken] = useState<string | null>(null);
+
+    useEffect(()=>{
+        const fetchMe =  async () => {
+            try {
+                const res = await api.getNewToken()
+                const newToken = res.token;
+                fetch.defaults.headers["Authorization"] = `Bearer ${newToken}`;
+                setToken(newToken);
+                const userData = await api.getUserMe()
+                setUser(userData.user)
+            }catch{
+                setToken(null)
+                setUser(null)
+            }
+        };
+        fetchMe()
+    },[]);
+
+    useLayoutEffect(()=>{
+        fetch.defaults.headers["Authorization"] = token ? `Bearer ${token}` : "";
+    },[token])
 
     const login = (userData: User, token: string) => {
         setUser(userData);
