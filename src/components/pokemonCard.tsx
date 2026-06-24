137d8be1186e7352fbcd4d5028c9b43fd5823df1
Bastien Antraygues <bastien-toulouse@outlook.fr>
2026-06-24 19:09:50 +0200

    Besoin de correction, voir si on reçoit bien les favorites pokemon dans favorites. Actuellement affiche les coeurs lorsqu'on est connecter, n'affiche pas encore les coeurs des pokemons en favorites

--- src/components/pokemonCard.tsx
+++ src/components/pokemonCard.tsx
@@ -1,22 +1,27 @@
 import React, { useEffect, useState } from "react"
 import type { Pokemon } from "../interfaces/Pokemon"
 import { Type } from "./Type"
-import { useFavorites } from "./FavoritesProvider"
+import { useFavorites } from "../providers/FavoritesProvider"
 import { upperName } from "./FunctionSpecial"
 import { useNavigate } from "react-router-dom"
 import api from "../services/api"
+import { useAuth } from "../providers/AuthProvider"
 
-export const PokemonCard = React.memo(function ({pokemon}: { pokemon: Pokemon }) {
-    const { favorite, setFavorite } = useFavorites()
+export const PokemonCard = React.memo(function ({ pokemon }: { pokemon: Pokemon }) {
+    const { user } = useAuth()
+    const { favorites, favoriteAdd, favorite, setFavorite } = useFavorites()
     const isFavorite = favorite.some(f => f.name == pokemon.name)
     const navigate = useNavigate()
-    
-    function toggleFavorite(){
-        if(isFavorite){
-            setFavorite(favorite.filter(item=>item.name!==pokemon.name))
-        }else{
-            setFavorite([...favorite,pokemon])
-        }
+
+    function toggleFavorite() {
+        if (favorites){
+            favoriteAdd(pokemon)
+        }/*
+        if (isFavorite) {
+            setFavorite(favorite.filter(item => item.name !== pokemon.name))
+        } else {
+            setFavorite([...favorite, pokemon])
+        }*/
     }
 
     if (!pokemon) {
@@ -24,9 +29,9 @@ export const PokemonCard = React.memo(function ({pokemon}: { pokemon: Pokemon })
             <>
 
                 <div className="m-4 p-4 border-2  text-center w-60 mx-auto border-pink-6 bg-pink-3 rounded-xl">
-                <h1 className="mt-8">Pokemon not find</h1>
+                    <h1 className="mt-8">Pokemon not find</h1>
 
-            </div>
+                </div>
 
             </>
         )
@@ -38,13 +43,19 @@ export const PokemonCard = React.memo(function ({pokemon}: { pokemon: Pokemon })
 
             <div className="m-4 p-4 border-2  text-center w-60 mx-auto border-pink-6 bg-pink-3 rounded-xl">
                 <div className="text-right">
-                    <button
-                        className=""
-                        onClick={toggleFavorite}
-                        aria-label="Ajouter ou retirer des favoris"
-                    >
-                        {isFavorite ? "❤️" : "🤍"}
-                    </button>
+                    {user && (
+                        <>
+                            <button
+                                className=""
+                                onClick={toggleFavorite}
+                                aria-label="Ajouter ou retirer des favoris"
+                            >
+                                {isFavorite ? "❤️" : "🤍"}
+                            </button>
+                        </>
+
+                    )}
+
                 </div>
                 <div onClick={() => navigate("/detail/" + pokemon.name)} className=" p-2 border-pink-8 rounded-lg hover:shadow-2xl">
                     <h1 className="">{upperName(pokemon.name)}</h1>
@@ -52,7 +63,7 @@ export const PokemonCard = React.memo(function ({pokemon}: { pokemon: Pokemon })
                     <img className="mx-auto" src={pokemon.sprites.front_default} alt="" />
                     <p>N° {pokemon.order}</p>
                     <div className="flex">{pokemon.types.map((element) => {
-                        return element?.type?.name ? <Type key={element.type.name} name={element.type.name}  />: null
+                        return element?.type?.name ? <Type key={element.type.name} name={element.type.name} /> : null
                     })}</div>
                 </div>
 
